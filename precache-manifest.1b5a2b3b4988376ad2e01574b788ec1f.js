4428d284a3387d0cb4246f8bfdd20867e6337dd9
Code Sandbox <hello@codesandbox.io>
2021-11-08 23:05:30 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1b5a2b3b4988376ad2e01574b788ec1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c58b804cd048785bc7b9e926cf270ff",
+    "url": "https://.github.io/csb-hf52s/index.html"
+  },
+  {
+    "revision": "80f299bed1689c84dd02",
+    "url": "https://.github.io/csb-hf52s/static/css/main.a1ca3e5c.chunk.css"
+  },
+  {
+    "revision": "7bdb3ab4397bdfc88edb",
+    "url": "https://.github.io/csb-hf52s/static/js/2.026437c1.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://.github.io/csb-hf52s/static/js/2.026437c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f299bed1689c84dd02",
+    "url": "https://.github.io/csb-hf52s/static/js/main.f7b8281e.chunk.js"
+  },
+  {
+    "revision": "204e1da067777711d4cd",
+    "url": "https://.github.io/csb-hf52s/static/js/runtime-main.7d4a72ad.js"
+  }
+]);
